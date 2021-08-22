1489c8cdd5399825d5703592278db6daf4b898f8
Zacstop <zaca3392@gmail.com>
2021-08-23 00:18:00 +0900

    study5

---
+++ src/routes/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./About.css";
+
+function About() {
+  return (
+    <div className="about__container">
+      <span>뭉탱이로있다가 유링게슝 아이그냥</span>
+    </div>
+  );
+}
+
+export default About;
